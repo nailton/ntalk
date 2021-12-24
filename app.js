0f81b019790f66c7763668df0713b534c85c6de6
nailton <nailton.osn@gmail.com>
2021-12-24 11:44:16 -0300

    refactory, add consign for autoload models, controllers and routes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/index');
-const users = require('./routes/users');
+const consign = require('consign');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view egine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/usuarios', users);
+consign({})
+.include('models')
+.then('controllers')
+.then('routes')
+.into(app);
 
 app.listen(3000, () => {
   console.log('Ntalk no ar!');
