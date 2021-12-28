9ca8f301a9b8badd0f1fb5271856066b8d814a38
nailton <nailton.osn@gmail.com>
2021-12-28 11:49:52 -0300

    crud contatos

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -2,7 +2,39 @@ module.exports = (app) => {
   const ContatosController = {
     index(req, res) {
       const { usuario } = req.session;
-      res.render('contatos/index', { usuario });
+      const { contatos } = req.usuario;
+      res.render('contatos/index', { usuario, contatos });
+    },
+    create(req, res) {
+      const { contato } = req.body;
+      const { usuario } = req.session;
+      usuario.contatos.push(contato);
+      res.redirect('/contatos');
+    },
+    show(req, res) {
+      const { id } = req.params;
+      const { usuario } = req.session;
+      const contato = usuario.contatos[id];
+      res.render('contatos/show', { id, contato })
+    },
+    edit(req, res) {
+      const { id } = req.params;
+      const { usuario } = req.session;
+      const contato = usuario.contatos[id];
+      res.render('contatos/edit', { id, contato, usuario });
+    },
+    update(req, res) {
+      const { contato } = req.body;
+      const { usuario } = req.session;
+      usuario.contatos[req.params.id] = contato;
+      res.redirect('/contatos');
+    },
+    destroy(req, res) {
+      const { id } = req.params;
+      const { usuario } = req.session;
+      usuario.contatos.splice(id, 1);
+      res.redirect('/contatos');
+
     }
   };
   return ContatosController;
