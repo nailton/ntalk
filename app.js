298af1b713f101f5c4606949d6fd6b6b586fccc8
nailton <nailton.osn@gmail.com>
2021-12-29 17:50:48 -0300

    wip socket.io

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -9,6 +11,8 @@ const error = require('./middlewares/error');
 
 
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,6 +28,14 @@ consign({})
   .then('controllers')
   .then('routes')
   .into(app);
+
+io.on('connection', (client) => {
+  client.on('send-server', (data) => {
+    const resposta = `<b>${data.nome}:</b> ${data.msg}<br>`;
+    client.emit('send-client', resposta);
+    client.broadcast.emit('send-client', resposta);
+  });
+});
 
 app.use(error.notFound);
 app.use(error.serverError)
