5fedd403070562386ee7abcaa1b5431c278fa596
nailton <nailton.osn@gmail.com>
2022-02-25 17:08:16 -0300

    notify script

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,7 +1,16 @@
+const crypto = require('crypto');
+
 module.exports = (app) => {
   const ChatController = {
     index(req, res) {
-      res.render('chat/index');
+      const { sala } = req.query;
+      let hashDaSala = sala;
+      if (!hashDaSala) {
+        const timestamp = Date.now().toString();
+        const md5 = crypto.createHash('md5');
+        hashDaSala = md5.update(timestamp).digest('hex');
+      }
+      res.render('chat/index', { sala: hashDaSala });
     }
   };
   return ChatController;
--- events/chat.js
+++ events/chat.js
@@ -3,10 +3,27 @@ module.exports = (app, io) => {
     const { session } = client.handshake;
     const { usuario } = session;
 
-    client.on('send-server', (msg) => {
+    client.on('send-server', (hashDaSala, msg) => {
+      const novaMensagem = { email: usuario.email, sala: hashDaSala};
       const resposta = `<b>${usuario.nome}:</b> ${msg}<br>`;
+      session.sala = hashDaSala;
+      client.broadcast.emit('new-message', novaMensagem);
+      io.to(hashDaSala).emit('send-client', resposta);
       client.emit('send-client', resposta);
       client.broadcast.emit('send-client', resposta);
     });
+
+    client.on('create-room', (hashDaSala) => {
+      session.sala = hashDaSala;
+      client.join(hashDaSala);
+    });
+
+    client.on('disconnect', () => {
+      const { sala } = session;
+      session.sala = null;
+      client.leave(sala);
+    });
+
+
   });
 }
